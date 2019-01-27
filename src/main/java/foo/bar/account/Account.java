99e000c34a9a95d40581aa4eaa49da40c4cfcf7d
Amewarashi-desu <amewarashidesu@gmail.com>
2019-01-27 19:44:39 +0300

    overriding methods

--- src/main/java/foo/bar/account/Account.java
+++ src/main/java/foo/bar/account/Account.java
@@ -2,6 +2,7 @@ package foo.bar.account;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 class Account {
 
@@ -18,6 +19,31 @@ class Account {
         this.id = id;
         this.balance = balance;
         this.created = created;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Account{");
+        sb.append("id=").append(id);
+        sb.append(", balance=").append(balance);
+        sb.append(", created=").append(created);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(id, account.id) &&
+                Objects.equals(balance, account.balance) &&
+                Objects.equals(created, account.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, balance, created);
     }
 
     public Long getId() {
